51ef6aeaaea8cc08029aebc3c6c282bc419938cc
k-sugisaki <60680331+k-sugisaki@users.noreply.github.com>
2020-07-07 09:12:48 +0900

    Add files via upload

---
+++ js/top.js
@@ -0,0 +1,1 @@
+window.onload=function(){const a=new Swiper(".swiper-container",{loop:true,autoplay:{delay:3000,disableOnInteraction:false},pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},keyboard:true})};
